3edfb5cc0bd2c85f98b1ffd39d40eda6fcd5ff5f
molor <yeahmolo@163.com>
2020-02-16 20:00:48 +0800

    update:modify webpack-base-test.js

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -7,7 +7,7 @@ describe('webpack.base.js test case', () => {
   // console.log(baseConfig);
   it('entry', () => {
     // 判断entry是否是合理的
-    assert.equal(baseConfig.entry.index, '/Users/lihua/Desktop/store/webpack-learn-code/super-builder-webpack/test/smoke/template/src/index/index.js')
-    assert.equal(baseConfig.entry.search, '/Users/lihua/Desktop/store/webpack-learn-code/super-builder-webpack/test/smoke/template/src/search/index.js')
+    assert.equal(baseConfig.entry.index.indexOf('super-builder-webpack/test/smoke/template/src/index/index.js') > 0, true );
+    assert.equal(baseConfig.entry.search.indexOf('super-builder-webpack/test/smoke/template/src/search/index.js') > 0, true);
   });
 });
